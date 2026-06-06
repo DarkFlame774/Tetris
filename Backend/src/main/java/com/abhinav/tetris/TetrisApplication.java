5d3118e5ae91b7f84ee1d46788bb6fc937f62566
DarkFlame774 <gargabhi1505@gmail.com>
2026-06-06 13:52:24 +0530

    Initialize SpringBoot for Backend

---
+++ Backend/src/main/java/com/abhinav/tetris/TetrisApplication.java
@@ -0,0 +1,13 @@
+package com.abhinav.tetris;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TetrisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TetrisApplication.class, args);
+	}
+
+}
